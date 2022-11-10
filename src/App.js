f113921b3712f7e4dffecc74297bb27c4b127a48
nahmad94 <deevan11@gmail.com>
2022-11-10 16:28:45 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+
+const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=15a205e299d84a7ca189f836671e2bd8'
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        accept: 'application/json'
+      }
+    })
+    .then((res) => res.json())
+    .then((json) => {
+      setData(json.articles)
+    })
+  }, [])
+
+  console.log(data)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {data.map((element, index) => {
+          return(
+            <li key={index}>
+              <img src={element.urlToImage}></img>
+              {element.title}</li>
+          )
+        })}
+      </ul>
     </div>
   );
 }

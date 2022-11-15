bc2ed7a121b91372d3456b30b562e4265429b54e
nahmad94 <deevan11@gmail.com>
2022-11-15 16:24:29 -0500

    adds search component and button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './Components/Header'
 import Headlines from './Components/Headlines'
 import Article from './Components/Article'
 import Footer from './Components/Footer'
+import Search from './Components/Search'
 
 const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=15a205e299d84a7ca189f836671e2bd8'
 
@@ -35,6 +36,7 @@ function App() {
         <Routes>
           <Route path='/' element={ <Headlines data={data}/>} />
           <Route path='article/:articleNumber' element={ <Article data={data} />} />
+          <Route path='/Search' element={ <Search /> } />
         </Routes>
       </main>
       <Footer />
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import Search from './Search'
 
 export default function Header() {
   return (
     <div className='navbar'>
         <nav className='footnote'>
             <h1 className='header'>NewsApp</h1>
+            <Link to='/Search'>
+              <Search />
+            </Link>
         </nav>
     </div>
   )

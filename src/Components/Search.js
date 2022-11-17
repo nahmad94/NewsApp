1f9aa61ebbbf4cf0cc50d9eda745449604458f28
nahmad94 <deevan11@gmail.com>
2022-11-17 09:26:37 -0500

    adds link functionality to search/broken

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Search() {
     const searchURL = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=15a205e299d84a7ca189f836671e2bd8'
@@ -23,8 +24,12 @@ export default function Search() {
         {searchResults.map((element, index) => {
             return(
                 <div>
-                    <h1>{element.title}</h1>
-                    <h3>{element.author}</h3>
+                    <Link to={element.url}>
+                    <div>
+                        <h1>{element.title}</h1>
+                        <h3>{element.author}</h3>
+                    </div>
+                    </Link>
                 </div>
             )
         })}
